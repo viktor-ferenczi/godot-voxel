e042b76bbf5bbae379d392b0791b07fd24bae4a1
Viktor Ferenczi <viktor@ferenczi.eu>
2023-03-25 01:55:35 +0100

    TexturedVoxelModel script update

--- addons/voxel/Scenes/TexturedVoxelModel.gd
+++ addons/voxel/Scenes/TexturedVoxelModel.gd
@@ -10,27 +10,7 @@ class_name TexturedVoxelModel
 		voxel_size = value
 		update()
 
-@export var size_in_cubes: Vector3i:
-	get:
-		return size_in_cubes
-	set(value):
-		size_in_cubes = value
-		update()
-
-@export var model_texture: Texture2D:
-	get:
-		return model_texture
-	set(value):
-		model_texture = value
-		update()
-
-@export var voxel_textures: CompressedTexture2DArray:
-	get:
-		return voxel_textures
-	set(value):
-		voxel_textures = value
-		update()
-
+# Texture UV scale: 16.0 to cover a single voxel with the texture
 @export_range(1.0, 16.0, 0.125) var uv_scale: float = 16.0:
 	get:
 		return uv_scale
@@ -38,6 +18,45 @@ class_name TexturedVoxelModel
 		uv_scale = value
 		update()
 
+# Number of voxel cubes (16x16x16 voxels each) in the model along each axis
+# Model size in individual voxels is 16 times larger than this vector.
+@export var model_size: Vector3i:
+	get:
+		return model_size
+	set(value):
+		model_size = value
+		update()
+
+# Coarse grained voxel cube data (level-1 DDA)
+# 256 x 16, RGB8
+@export var model_texture: Texture2D:
+	get:
+		return model_texture
+	set(value):
+		model_texture = value
+		update()
+
+# Fine grained voxel data (level-2 DDA)
+# 256 x 16 x N, R8
+@export var voxel_textures: CompressedTexture2DArray:
+	get:
+		return voxel_textures
+	set(value):
+		voxel_textures = value
+		update()
+
+# Palette defining what texture to render for each of the voxel values.
+# The first row is unused, because that corresponds to the empty voxel (zero).
+# Each row configures all 6 faces for the given voxel value.
+# 6 x 256, RGBA8
+@export var palette_texture: Texture2D:
+	get:
+		return palette_texture
+	set(value):
+		palette_texture = value
+		update()
+
+# Surface color for opaque, transmittance for transparent voxels (RGBA)
 @export var color_textures: CompressedTexture2DArray:
 	get:
 		return color_textures
@@ -45,6 +64,7 @@ class_name TexturedVoxelModel
 		color_textures = value
 		update()
 
+# Surface emission for opaque, volumetric emission for transparent voxels (RGB)
 @export var emission_textures: CompressedTexture2DArray:
 	get:
 		return emission_textures
@@ -52,6 +72,7 @@ class_name TexturedVoxelModel
 		emission_textures = value
 		update()
 
+# Normal map (XY) and depth (Z)
 @export var normal_textures: CompressedTexture2DArray:
 	get:
 		return normal_textures
@@ -59,6 +80,7 @@ class_name TexturedVoxelModel
 		normal_textures = value
 		update()
 
+# RSMA: Roughness (R), Specular (G), Metallic (B), Ambient occlusion (A)
 @export var rsma_textures: CompressedTexture2DArray:
 	get:
 		return rsma_textures
@@ -73,9 +95,6 @@ func update():
 
 # True if _ready has completed
 var is_ready: bool = false
-
-# True if all the input variables are valid
-var is_valid: bool = false
 
 # Shader material for each of the opaque, transparent and shadow nodes.
 # Separate nodes and materials are used, because the three rendering
@@ -96,29 +115,34 @@ func _ready():
 	$Transparent.owner = self
 	$Shadow.owner = self
 
+	# Configure on adding to the scene tree
 	configure()
 
+	# Allow for reconfiguration on property changes
 	is_ready = true
 
 
+# Clear for reuse in a pool
 func clear():
-	is_valid = false
-	size_in_cubes = Vector3i(0, 0, 0)
 	model_texture = null
 	voxel_textures = null
+	palette_texture = null
 	color_textures = null
 	emission_textures = null
 	normal_textures = null
 	rsma_textures = null
+	model_size = Vector3i(0, 0, 0)
+	voxel_size = 1.0
+	uv_scale = 16.0
+	set_node_scales()
 	set_shader_parameters()
 
 
 func configure():
-	is_valid = false
-
 	var is_incomplete = (
 		model_texture == null or
 		voxel_textures == null or
+		palette_texture == null or
 		color_textures == null or
 		emission_textures == null or
 		normal_textures == null or
@@ -127,109 +151,72 @@ func configure():
 	if is_incomplete:
 		return
 
-	var is_valid_size_in_cubes = (
-		size_in_cubes[size_in_cubes.min_axis_index()] >= 1 and
-		size_in_cubes[size_in_cubes.max_axis_index()] <= 16
+	var is_valid_size_in_cubes: bool = (
+		model_size[model_size.min_axis_index()] >= 1 and
+		model_size[model_size.max_axis_index()] <= 16
 	)
+
+	if Engine.is_editor_hint():
+		if not is_valid_size_in_cubes:
+			print("%s: Invalid model_size. Must be between (1, 1, 1) and (16, 16, 16)" % [name])
+		elif not expensive_verifications():
+			return
+
 	if not is_valid_size_in_cubes:
-		print("%s: Invalid size_in_cubes, must be between (1, 1, 1) and (16, 16, 16)" % [name])
 		return
 
-	var is_valid_model = (
-		size_in_cubes[size_in_cubes.min_axis_index()] >= 1 and
-		size_in_cubes[size_in_cubes.max_axis_index()] <= 16 and
-		model_texture != null and
-		#model_texture.get_format() == Image.FORMAT_RGB8 and
-		model_texture.get_width() == 16 and
-		model_texture.get_height() == 256
-	)
-	if not is_valid_model:
-		print("%s: Invalid model_texture, must be 16x256 FORMAT_RGB8" % [name])
+	set_node_scales()
+	set_shader_parameters()
 
-	var cube_count = size_in_cubes.x * size_in_cubes.y * size_in_cubes.z
-	var is_valid_voxel_textures = (
-		voxel_textures != null and
-		#voxel_textures.get_format() == Image.FORMAT_R8 and
-		voxel_textures.get_width() == 256 and
-		voxel_textures.get_height() == 16 and
-		voxel_textures.get_layers() == cube_count
-	)
-	if not is_valid_voxel_textures:
-		print("%s: Invalid voxel_textures, must be 256x16 FORMAT_R8 imported as W*H*D layers where size_in_cubes=(W, H, D)" % [name])
 
-	var w = color_textures.get_width()
-	var h = color_textures.get_height()
-	var l = color_textures.get_layers()
-
-	var is_valid_color_textures = (
-		color_textures != null and
-		#color_textures.get_format() == Image.FORMAT_RGBA8 and
-		w >= 1 and
-		w <= 512 and
-		h >= 1 and
-		h <= 512 and
-		l >= 2 and
-		l <= 256
-	)
-	if not is_valid_color_textures:
-		print("%s: Invalid color_textures, must be FORMAT_RGBA8 imported as 2..256 layers" % [name])
-
-	var is_valid_emission_textures = (
-		emission_textures != null and
-		#emission_textures.get_format() == Image.FORMAT_RGB8 and
-		emission_textures.get_width() == w and
-		emission_textures.get_height() == h and
-		emission_textures.get_layers() == l
-	)
-	if not is_valid_emission_textures:
-		print("%s: Invalid emission_textures, must be FORMAT_RGB8 imported as 2..256 layers" % [name])
-
-	var is_valid_normal_textures = (
-		normal_textures != null and
-		#normal_textures.get_format() == Image.FORMAT_RGB8 and
-		normal_textures.get_width() == w and
-		normal_textures.get_height() == h and
-		normal_textures.get_layers() == l
-	)
-	if not is_valid_normal_textures:
-		print("%s: Invalid normal_textures, must be FORMAT_RGB8 imported as 2..256 layers" % [name])
-
-	var is_valid_rsma_textures = (
-		rsma_textures != null and
-		#rsma_textures.get_format() == Image.FORMAT_RGBA8 and
-		rsma_textures.get_width() == w and
-		rsma_textures.get_height() == h and
-		rsma_textures.get_layers() == l
-	)
-	if not is_valid_rsma_textures:
-		print("%s: Invalid rsma_textures, must be FORMAT_RGBA8 imported as 2..256 layers" % [name])
-
-	is_valid = (
-		is_valid_model and
-		is_valid_voxel_textures and
-		is_valid_color_textures and
-		is_valid_emission_textures and
-		is_valid_normal_textures and
-		is_valid_rsma_textures
-	)
-	if not is_valid:
-		return
-
-	var scale = voxel_size * size_in_cubes * 16
+func set_node_scales():
+	var scale = voxel_size * model_size * 16
 	$Opaque.scale = scale
 	$Transparent.scale = scale
 	$Shadow.scale = scale
 
-	set_shader_parameters()
-
 
 func set_shader_parameters():
 	for shader_material in shader_materials:
-		shader_material.set_shader_parameter("size_in_cubes", size_in_cubes)
+		shader_material.set_shader_parameter("uv_scale", uv_scale)
+		shader_material.set_shader_parameter("model_size", model_size)
 		shader_material.set_shader_parameter("model_texture", model_texture)
 		shader_material.set_shader_parameter("voxel_textures", voxel_textures)
-		shader_material.set_shader_parameter("uv_scale", uv_scale)
+		shader_material.set_shader_parameter("palette_texture", palette_texture)
 		shader_material.set_shader_parameter("color_textures", color_textures)
 		shader_material.set_shader_parameter("emission_textures", emission_textures)
 		shader_material.set_shader_parameter("normal_textures", normal_textures)
 		shader_material.set_shader_parameter("rsma_textures", rsma_textures)
+
+
+func expensive_verifications():
+	var model_image: Image = model_texture.get_image()
+	var is_valid_model: bool = (
+		model_image.get_format() == Image.FORMAT_RGB8 and
+		not model_image.is_compressed() and
+		model_texture.get_width() == 256 and
+		model_texture.get_height() == 16
+	)
+	if not is_valid_model:
+		print("%s: Invalid model_texture. Must be 256x16 FORMAT_RGB8" % [name])
+
+	var voxel_cube_0: Image = voxel_textures.get_layer_data(0)
+	var is_valid_voxel: bool = (
+		not voxel_cube_0.is_compressed() and
+		voxel_textures.get_width() == 256 and
+		voxel_textures.get_height() == 16
+	)
+	if not is_valid_voxel:
+		print("%s: Invalid voxel_textures. Must be 256x16xN FORMAT_R8" % [name])
+
+	var palette_image = palette_texture.get_image()
+	var is_valid_palette: bool = (
+		palette_image.get_format() == Image.FORMAT_RGB8 and
+		not palette_image.is_compressed() and
+		palette_texture.get_width() == 6 and
+		palette_texture.get_height() == 256
+	)
+	if not is_valid_model:
+		print("%s: Invalid palette_texture. Must be 6x256 FORMAT_RGBA8" % [name])
+
+	return is_valid_model and is_valid_voxel and is_valid_palette
