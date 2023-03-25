fa8973ed3fa0db756877a4d7a57385d42368deb5
Viktor Ferenczi <viktor@ferenczi.eu>
2023-03-25 02:40:05 +0100

    Better material selection

--- addons/voxel/Examples/TorusBox/Assets/build.bat
+++ addons/voxel/Examples/TorusBox/Assets/build.bat
@@ -2,4 +2,3 @@ set PYTHON=python
 set TOOLS=..\..\..\Tools
 %PYTHON% -u -OO %TOOLS%\vox_to_images.py Vox/Slice.png Vox/Palette.png Voxel.png Model.png 64 80 48
 %PYTHON% -u -OO %TOOLS%\build_textures.py palette.json ../../Materials Palette.png Color.png Emission.png Normal.png RSMA.png 512
-pause
